ddf3d9571c1dd449a7ae0bd4c118c4afd6f1f550
Denis Popov <aoh@list.ru>
2022-10-27 22:38:57 +0300

    Version 1.4.2;

--- Xcode/flac/include/config.h
+++ Xcode/flac/include/config.h
@@ -249,7 +249,7 @@
 #define PACKAGE_NAME "flac"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "flac 1.4.1"
+#define PACKAGE_STRING "flac 1.4.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "flac"
@@ -258,7 +258,7 @@
 #define PACKAGE_URL "https://www.xiph.org/flac/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.4.1"
+#define PACKAGE_VERSION "1.4.2"
 
 /* The size of `off_t', as computed by sizeof. */
 /* #undef SIZEOF_OFF_T */
@@ -302,7 +302,7 @@
 
 
 /* Version number of package */
-#define VERSION "1.4.1"
+#define VERSION "1.4.2"
 
 /* Target processor is big endian. */
 #define WORDS_BIGENDIAN CPU_IS_BIG_ENDIAN
