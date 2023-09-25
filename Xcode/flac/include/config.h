7c62804ef10204387df88fe7361bdf4ca25de122
Denis Popov <aoh@list.ru>
2023-09-25 20:20:27 +0300

    Update config.h;

--- Xcode/flac/include/config.h
+++ Xcode/flac/include/config.h
@@ -4,6 +4,28 @@
 /* Define if building universal (internal helper macro) */
 #define AC_APPLE_UNIVERSAL_BUILD 1
 
+/* Target processor is big endian. */
+/* #undef CPU_IS_BIG_ENDIAN */
+
+/* Target processor ARM64 */
+/* #undef FLAC__CPU_ARM64 */
+
+/* Set FLAC__BYTES_PER_WORD to 8 (4 is the default) */
+/* #undef ENABLE_64_BIT_WORDS */
+
+/* define to align allocated memory on 32-byte boundaries */
+/* #undef FLAC__ALIGN_MALLOC_DATA */
+
+/* define if you have docbook-to-man or docbook2man */
+/* #undef FLAC__HAS_DOCBOOK_TO_MAN */
+
+/* define if you have the ogg library */
+#define OGG_FOUND 0
+#define FLAC__HAS_OGG OGG_FOUND
+
+/* define if you have pandoc */
+/* #undef FLAC__HAS_PANDOC */
+
 #if defined(__BIG_ENDIAN__)
  #define CPU_IS_BIG_ENDIAN 1
  #define CPU_IS_LITTLE_ENDIAN 0
@@ -11,18 +33,6 @@
  #define CPU_IS_LITTLE_ENDIAN 1
  #define CPU_IS_BIG_ENDIAN 0
 #endif
-
-/* Target processor is big endian. */
-/* #undef CPU_IS_BIG_ENDIAN */
-
-/* Target processor is little endian. */
-/* #undef CPU_IS_LITTLE_ENDIAN */
-
-/* Set FLAC__BYTES_PER_WORD to 8 (4 is the default) */
-/* #undef ENABLE_64_BIT_WORDS */
-
-/* define to align allocated memory on 32-byte boundaries */
-/* #undef FLAC__ALIGN_MALLOC_DATA */
 
 #if defined(__aarch64__)
  #define FLAC__CPU_ARM64 1
@@ -72,18 +82,8 @@
 /* define if building for arm64 */
 /* #undef FLAC__CPU_ARM64 */
 
-/* define if you have docbook-to-man or docbook2man */
-/* #undef FLAC__HAS_DOCBOOK_TO_MAN */
-
 /* define if you are compiling for x86 and have the NASM assembler */
 /* #undef FLAC__HAS_NASM */
-
-/* define if you have the ogg library */
-#define OGG_FOUND 0
-#define FLAC__HAS_OGG OGG_FOUND
-
-/* define if you have pandoc */
-/* #undef FLAC__HAS_PANDOC */
 
 /* define if compiler has __attribute__((target("cpu=power8"))) support */
 /* #undef FLAC__HAS_TARGET_POWER8 */
@@ -99,9 +99,6 @@
 
 /* Set to 1 if <arm_neon.h> contains A64 intrinsics */
 /* #undef FLAC__HAS_A64NEONINTRIN */
-
-/* define to disable use of assembly code */
-/* #undef FLAC__NO_ASM */
 
 /* define if building for Darwin / MacOS X */
 #define FLAC__SYS_DARWIN 1
@@ -145,17 +142,11 @@
 /* Define to 1 if you have the <cpuid.h> header file. */
 /* #undef HAVE_CPUID_H */
 
-/* Define to 1 if you have the <dlfcn.h> header file. */
-#define HAVE_DLFCN_H 1
-
 /* Define to 1 if fseeko (and presumably ftello) exists and is declared. */
 #define HAVE_FSEEKO 1
 
-/* Define to 1 if you have the `getauxval' function. */
-/* #undef HAVE_GETAUXVAL */
-
 /* Define to 1 if you have the `getopt_long' function. */
-#define HAVE_GETOPT_LONG 1
+/* #undef HAVE_GETOPT_LONG */
 
 /* Define if you have the iconv() function and it works. */
 #define HAVE_ICONV 1
@@ -169,29 +160,20 @@
 /* lround support */
 #define HAVE_LROUND 1
 
-/* Define to 1 if you have the <minix/config.h> header file. */
-/* #undef HAVE_MINIX_CONFIG_H */
+/* Define to 1 if you have the <memory.h> header file. */
+/* #undef HAVE_MEMORY_H */
 
-/* Define to 1 if the system has the type `socklen_t'. */
-/* #undef HAVE_SOCKLEN_T */
+/* Define if multithreading is enabled */
+#define HAVE_PTHREAD 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
 
-/* Define to 1 if you have the <stdio.h> header file. */
-#define HAVE_STDIO_H 1
-
 /* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
-
-/* Define to 1 if you have the <strings.h> header file. */
-#define HAVE_STRINGS_H 1
+/* #undef HAVE_STDLIB_H */
 
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
-
-/* Define to 1 if you have the <sys/auxv.h> header file. */
-/* #undef HAVE_SYS_AUXV_H */
 
 /* Define to 1 if you have the <sys/ioctl.h> header file. */
 #define HAVE_SYS_IOCTL_H 1
@@ -200,31 +182,25 @@
 #define HAVE_SYS_PARAM_H 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H 1
+/* #undef HAVE_SYS_STAT_H */
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H 1
+/* #undef HAVE_SYS_TYPES_H */
 
 /* Define to 1 if you have the <termios.h> header file. */
 #define HAVE_TERMIOS_H 1
 
 /* Define to 1 if typeof works with your compiler. */
-#define HAVE_TYPEOF 1
+/* #undef HAVE_TYPEOF */
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H 1
-
-/* Define to 1 if you have the <wchar.h> header file. */
-#define HAVE_WCHAR_H 1
+/* #undef HAVE_UNISTD_H */
 
 /* Define to 1 if you have the <x86intrin.h> header file. */
 /* #undef HAVE_X86INTRIN_H */
 
 /* Define as const if the declaration of iconv() needs const. */
-#define ICONV_CONST 
-
-/* Define to the sub-directory where libtool stores uninstalled libraries. */
-#define LT_OBJDIR ".libs/"
+/* #undef ICONV_CONST */
 
 /* Define if debugging is disabled */
 /* #undef NDEBUG */
@@ -256,11 +232,6 @@
 /* The size of `void*', as computed by sizeof. */
 /* #undef SIZEOF_VOIDP */
 
-/* Define to 1 if all of the C90 standard headers exist (not just the ones
-   required in a freestanding environment). This macro is provided for
-   backward compatibility; new code need not use it. */
-#define STDC_HEADERS 1
-
 /* Enable extensions on AIX 3, Interix.  */
 #ifndef _ALL_SOURCE
 # define _ALL_SOURCE 1
@@ -269,30 +240,34 @@
 #ifndef _DARWIN_C_SOURCE
 # define _DARWIN_C_SOURCE 1
 #endif
-/* Enable general extensions on Solaris.  */
-#ifndef __EXTENSIONS__
-# define __EXTENSIONS__ 1
-#endif
 /* Enable GNU extensions on systems that have them.  */
 #ifndef _GNU_SOURCE
 # define _GNU_SOURCE 1
 #endif
+
+#ifndef _XOPEN_SOURCE
+/* #undef DODEFINE_XOPEN_SOURCE */
+#ifdef DODEFINE_XOPEN_SOURCE
+#define _XOPEN_SOURCE DODEFINE_XOPEN_SOURCE
+#endif
+#endif
+
 /* Enable threading extensions on Solaris.  */
 #ifndef _POSIX_PTHREAD_SEMANTICS
-# define _POSIX_PTHREAD_SEMANTICS 1
+/* #undef _POSIX_PTHREAD_SEMANTICS */
 #endif
 /* Enable extensions on HP NonStop.  */
 #ifndef _TANDEM_SOURCE
-# define _TANDEM_SOURCE 1
+/* #undef _TANDEM_SOURCE */
 #endif
 /* Enable general extensions on Solaris.  */
 #ifndef __EXTENSIONS__
-# define __EXTENSIONS__ 1
+#define DODEFINE_EXTENSIONS
+#ifdef DODEFINE_EXTENSIONS
+#define __EXTENSIONS__ DODEFINE_EXTENSIONS
+#endif
 #endif
 
-
-/* Version number of package */
-#define VERSION "1.4.3"
 
 /* Target processor is big endian. */
 #define WORDS_BIGENDIAN CPU_IS_BIG_ENDIAN
@@ -324,12 +299,6 @@
 
 /* Define to 1 if you need to in order for `stat' and other things to work. */
 /* #undef _POSIX_SOURCE */
-
-/* Define to `__inline__' or `__inline' if that's what the C compiler
-   calls it, or to nothing if 'inline' is not supported under any name.  */
-#ifndef __cplusplus
-/* #undef inline */
-#endif
 
 /* Define to __typeof__ if your compiler spells it that way. */
 /* #undef typeof */
